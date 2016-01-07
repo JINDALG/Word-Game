a3c4b92c090962976b4850da6d17a90da4764e7f
Shivam jindal <jindalshivam65@gmail.com>
2016-01-08 01:41:09 +0530

    model migrate

--- wordgame/models.py
+++ wordgame/models.py
@@ -5,10 +5,22 @@ import datetime
 
 # Create your models here.
 
-class word_gane_quiz(models.Model):
-	main_word = models.charField(max_length=20)
-	question = models.charField(max_length=100)
-	sentence = models.charField(max_length=200)
-	meaning = models.charField(max_length=100)
-	image_url = models.charField(max_length=100)
+class Quiz(models.Model):
+	main_word = models.CharField(max_length=20)
+	question  = models.CharField(max_length=100)
+	sentence  = models.CharField(max_length=200)
+	meaning   = models.CharField(max_length=100)
+	image_url = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.main_word
+
+
+class User(models.Model):
+	user_name = models.CharField(max_length=20)
+	password = models.CharField(max_length=20)
+	score = models.IntegerField(default =0)
+
+	def __unicode__(self):
+		return self.user_name
 
--- wordgame/views.py
+++ wordgame/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Quiz,User
+from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request,user_name):
+	if request.method == 'POST':
+
+	else :
+		render(request,wordgame/index.html)
+
+
